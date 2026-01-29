6164e63c3d453cb2aed5a06fc05a716f8c497175
kabinet303 <kabinet303@localhost.localdomain>
2026-01-29 13:20:58 +0300

    создание проекта

---
+++ mainNNV/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MainkdrConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mainNNV'
---
+++ mainNNV/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def mainfNNV (request):
+    return HttpResponse("app mainNNV") 
+
+# Create your views here.
